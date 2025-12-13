3c7eaf5aa9cc1dae4b5e796eae0283293995df21
教主 <2026482602@qq.com>
2025-12-13 20:08:56 +0800

    修复

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,69 +4,8 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-<<<<<<< ours
-  header: [
-    Component.Flex({
-      wrap: "wrap",
-      gap: "0.75rem",
-      components: [
-        { Component: Component.PageTitle(), shrink: false },
-        {
-          Component: Component.SiteNav({
-            links: [
-              { label: "开始", slug: "start-here" },
-              { label: "文章", slug: "posts/" },
-              { label: "笔记", slug: "notes/" },
-              { label: "项目", slug: "projects/" },
-              { label: "近况", slug: "now" },
-              { label: "关于", slug: "about" },
-            ],
-          }),
-          grow: true,
-        },
-        { Component: Component.Search(), grow: true, basis: "14rem" },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-  ],
-  afterBody: [
-    Component.ConditionalRender({
-      condition: (page) => page.fileData.slug === "index",
-      component: Component.Flex({
-        wrap: "wrap",
-        gap: "2rem",
-        components: [
-          {
-            Component: Component.RecentNotes({
-              title: "最新文章",
-              limit: 5,
-              showTags: false,
-              linkToMore: "posts/" as SimpleSlug,
-              filter: (f) => (f.slug?.startsWith("posts/") ?? false) && f.slug !== "posts/index",
-            }),
-            grow: true,
-            basis: "22rem",
-          },
-          {
-            Component: Component.RecentNotes({
-              title: "最新笔记",
-              limit: 5,
-              showTags: false,
-              linkToMore: "notes/" as SimpleSlug,
-              filter: (f) => (f.slug?.startsWith("notes/") ?? false) && f.slug !== "notes/index",
-            }),
-            grow: true,
-            basis: "22rem",
-          },
-        ],
-      }),
-    }),
-  ],
-=======
   header: [],
   afterBody: [],
->>>>>>> theirs
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
